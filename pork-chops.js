aaad77ab298f067dd2e43ad61241236348104a0b
Pigmine <pigmine@yahoo.com>
2010-05-06 21:00:17 -0700

    fixed issues added comments

--- pork-chops.js
+++ pork-chops.js
@@ -1,6 +1,10 @@
 (function (window) {
+	//save the current Chops variable in case we are overwriting someone else's chops
 	var old_Chops = window.Chops;
 	
+	//serves as the basis of object mixing
+	//overwrites properties in one object with a different's object's properties
+	//does NOT include prototypes of objects in copying
 	function mergeObjects(destination, source) {
 		for (var property in source) {
 			if (source.hasOwnProperty(property))
@@ -9,7 +13,11 @@
 		return destination;
 	}
 	
+	//Chops namespace - call noConflict to remove from global namespace
 	window.Chops = {
+		//pass in objects or functions as arguments
+		//objects (without their prototypes) and objects returned by functions passed in as arguments are mixed
+		//the properties of the first argument takes precedence over the next, and so on
 		mix: function() {
 			var o;
 			
@@ -20,15 +28,17 @@
 					o = mergeObjects(o, arguments[i]);
 			}
 			
-			return F;
+			return o;
 		},
 		
+		//removes Chops from the global namespace and restores any previous value for Chops
 		noConflict: function () {
 			var c = window.Chops;
-			window.Chops = old_Class;
+			window.Chops = old_Chops;
 			return c;
 		},
 		
+		//serves as a namespace for prepared functions as an extension to Chops
 		mixins: {}
 	};
 })(this);
