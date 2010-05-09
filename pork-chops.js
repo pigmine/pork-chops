bcfbfdecf13df36932f77e4cb5df5d85f09f68d7
Pigmine <pigmine@yahoo.com>
2010-05-09 10:39:04 -0700

    Simplified Chops.mix

--- pork-chops.js
+++ pork-chops.js
@@ -1,31 +1,26 @@
-(function (window) {
+(function () {
 	//save the current Chops variable in case we are overwriting someone else's chops
-	var old_Chops = window.Chops;
-	
-	//serves as the basis of object mixing
-	//overwrites properties in one object with a different's object's properties
-	//does NOT include prototypes of objects in copying
-	function mergeObjects(destination, source) {
-		for (var property in source) {
-			if (source.hasOwnProperty(property))
-				destination[property] = source[property];
-		}
-		return destination;
-	}
+	var old_Chops = this.Chops;
 	
 	//Chops namespace - call noConflict to remove from global namespace
-	window.Chops = {
+	this.Chops = {
 		//pass in objects or functions as arguments
 		//objects (without their prototypes) and objects returned by functions passed in as arguments are mixed
 		//the properties of the first argument takes precedence over the next, and so on
 		mix: function() {
-			var o;
+			var o = {}
+				,property
+				,i
+				,c;
 			
-			for (var i = arguments.length - 1; i >= 0; i -= 1) {
-				if (typeof arguments[i] === 'function')
-					o = mergeObjects(o, arguments[i]());
-				else
-					o = mergeObjects(o, arguments[i]);
+			for (i = arguments.length; i >= 0; i -= 1) {
+				c = arguments[i];
+				if (typeof c === 'function')
+					c = c();
+				for (property in c) {
+					if (c.hasOwnProperty(property))
+						o[property] = c[property];
+				}
 			}
 			
 			return o;
@@ -33,14 +28,12 @@
 		
 		//removes Chops from the global namespace and restores any previous value for Chops
 		noConflict: function () {
-			var c = window.Chops;
-			window.Chops = old_Chops;
+			var c = Chops;
+			Chops = old_Chops;
 			return c;
 		},
-		
-		mergeObjects: mergeObjects,
 		
 		//serves as a namespace for prepared functions as an extension to Chops
 		mixins: {}
 	};
-})(this);
+})();
