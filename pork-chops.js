0646f145845fc937e1d91796e0475ec7ae1b2b23
Pigmine <pigmine@yahoo.com>
2010-05-09 12:26:46 -0700

    merged all of chops into one file for consistency and ease of implementation

--- pork-chops.js
+++ pork-chops.js
@@ -35,6 +35,65 @@
 		},
 		
 		//serves as a namespace for prepared functions as an extension to Chops
-		mixins: {}
+		mixins: {
+			event: {
+				_events_Chops_: {},
+				
+				bind: function (name, fn) {
+					if (!this._events_Chops_[name])
+						this._events_Chops_[name] = [];
+					
+					function f() {
+						fn.apply(this);
+					}
+					
+					f._fn_Chops_ = fn;
+					
+					this._events_Chops_.push(f);
+					
+					return this;
+				},
+				
+				unbind: function (name, fn) {
+					var a = this._events_Chops_[name];
+					if (!!a && a.length) {
+						for (var i = 0, len = a.length; i < len; i++) {
+							if (a[i]._fn_Chops_ === fn)
+								a.splice(i, 1);
+								break;
+						}
+					}
+					this._events_Chops_[name] = a;
+					
+					return this;
+				},
+				
+				trigger: function (name) {
+					var a = this._events_Chops_[name];
+					if (!!a && a.length) {
+						for (var i = 0, len = a.length; i < len; i++) {
+							a[i]();
+						}
+					}
+					
+					return this;
+				}
+			},
+			
+			mixer: {
+				mixin: function () {
+					var property, i, c;
+					for (i = arguments.length; i >= 0; i -= 1) {
+						c = arguments[i];
+						if (typeof c === 'function')
+							c = c();
+						for (property in c) {
+							if (c.hasOwnProperty(property))
+								this[property] = c[property];
+						}
+					}
+				}
+			}
+		}
 	};
 })(this);
