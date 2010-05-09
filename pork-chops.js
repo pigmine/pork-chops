5b25d9b6e5766a0955050fe885c1059291c8dfd1
Pigmine <pigmine@yahoo.com>
2010-05-09 10:58:08 -0700

    Added window object to closure

--- pork-chops.js
+++ pork-chops.js
@@ -1,39 +1,40 @@
-(function () {
+(function (window) {
 	//save the current Chops variable in case we are overwriting someone else's chops
-	var old_Chops = this.Chops;
+	var old_Chops = window.Chops;
 	
 	//Chops namespace - call noConflict to remove from global namespace
-	this.Chops = {
+	var Chops = window.Chops = {
 		//pass in objects or functions as arguments
 		//objects (without their prototypes) and objects returned by functions passed in as arguments are mixed
 		//the properties of the first argument takes precedence over the next, and so on
 		mix: function() {
-			var o = {}
-				,property
-				,i
-				,c;
+			var obj = {} //the new object we mix into
+				,property //holds the string for looping properties of objects
+				,i //holds the array counter for looping through arguments
+				,cur; //holds the current argument in the arguments loop for speed
 			
 			for (i = arguments.length; i >= 0; i -= 1) {
-				c = arguments[i];
-				if (typeof c === 'function')
-					c = c();
-				for (property in c) {
-					if (c.hasOwnProperty(property))
-						o[property] = c[property];
+				cur = arguments[i];
+				if (typeof cur === 'function')
+					cur = cur();
+				for (property in cur) {
+					//Chops does not mix in prototyped properties of an object
+					if (cur.hasOwnProperty(property))
+						obj[property] = cur[property];
 				}
 			}
 			
-			return o;
+			return obj;
 		},
 		
 		//removes Chops from the global namespace and restores any previous value for Chops
 		noConflict: function () {
-			var c = Chops;
-			Chops = old_Chops;
+			var c = window.Chops;
+			window.Chops = old_Chops;
 			return c;
 		},
 		
 		//serves as a namespace for prepared functions as an extension to Chops
 		mixins: {}
 	};
-})();
+})(this);
